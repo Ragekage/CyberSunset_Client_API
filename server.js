ca10af3ddc707aa0ae60c8501de1beef0692e9fe
Kade <kadedavies@hotmail.co.uk>
2020-04-25 18:32:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 var sql = require('mssql')
 var bodyParser = require('body-parser')
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 
 var nodemailer = require('nodemailer')
